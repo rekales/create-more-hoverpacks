c3ea7f39be0e702d9dd332559fa9886884a62e82
Krei <criketgun@gmail.com>
2026-02-23 02:29:28 +0800

    move some conditions around change flying speed when using hoverpack

--- src/main/java/com/kreidev/cmhoverpacks/FlightHandler.java
+++ src/main/java/com/kreidev/cmhoverpacks/FlightHandler.java
@@ -12,24 +12,22 @@ import static com.kreidev.cmhoverpacks.Hoverpacks.resLoc;
 
 public class FlightHandler {
 
-    public static final AttributeModifier FLIGHT_MODIFIER = new AttributeModifier(resLoc("hoverpack"), 1.0,
+    public static final AttributeModifier FLIGHT_MODIFIER = new AttributeModifier(resLoc("hoverpack_flight"), 1.0,
             AttributeModifier.Operation.ADD_VALUE);
-
 
     public static void onPlayerTick(PlayerTickEvent.Post event) {
         Player player = event.getEntity();
         Level level = player.level();
 
         if (!level.isClientSide) {
-            AttributeInstance flightAttr = player.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
-            if (flightAttr == null) return;
-
             if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof HoverpackItem) {
-                if (!flightAttr.hasModifier(FLIGHT_MODIFIER.id())) {
+                AttributeInstance flightAttr = player.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
+                if (flightAttr != null && !flightAttr.hasModifier(FLIGHT_MODIFIER.id())) {
                     flightAttr.addTransientModifier(FLIGHT_MODIFIER);
                 }
             } else {
-                if (flightAttr.hasModifier(FLIGHT_MODIFIER.id())) {
+                AttributeInstance flightAttr = player.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
+                if (flightAttr != null && flightAttr.hasModifier(FLIGHT_MODIFIER.id())) {
                     flightAttr.removeModifier(FLIGHT_MODIFIER.id());
                 }
             }
--- src/main/java/com/kreidev/cmhoverpacks/mixin/PlayerMixin.java
+++ src/main/java/com/kreidev/cmhoverpacks/mixin/PlayerMixin.java
@@ -8,6 +8,9 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.common.NeoForgeMod;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Player.class, remap = false)
 public abstract class PlayerMixin extends LivingEntity {
@@ -21,12 +24,21 @@ public abstract class PlayerMixin extends LivingEntity {
         Player self = (Player)(Object)this;
 
         AttributeInstance flightAttr = self.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
-        if (flightAttr != null) {
-            if (flightAttr.hasModifier(FlightHandler.FLIGHT_MODIFIER.id()) && self.getAbilities().flying) {
-                return;
-            }
+        if (flightAttr != null
+                && flightAttr.hasModifier(FlightHandler.FLIGHT_MODIFIER.id())
+                && self.getAbilities().flying) {
+            return;
         }
 
         super.setSprinting(sprinting);
+    }
+
+    @Inject(method = "getFlyingSpeed", at = @At("HEAD"), cancellable = true)
+    public void getFlyingSpeed(CallbackInfoReturnable<Float> cir) {
+        Player self = (Player)(Object)this;
+        AttributeInstance flightAttr = self.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
+        if (flightAttr != null && flightAttr.hasModifier(FlightHandler.FLIGHT_MODIFIER.id())) {
+            cir.setReturnValue(self.getAbilities().getFlyingSpeed() * 0.75f);
+        }
     }
 }
