3f3d3418317afb8254e2b222e1280f4ddf88f1c5
Krei <criketgun@gmail.com>
2026-02-24 00:16:56 +0800

    add air requirement and handling

--- src/main/java/com/kreidev/cmhoverpacks/FlightHandler.java
+++ src/main/java/com/kreidev/cmhoverpacks/FlightHandler.java
@@ -1,12 +1,19 @@
 package com.kreidev.cmhoverpacks;
 
+import com.simibubi.create.content.equipment.armor.BacktankItem;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.NeoForgeMod;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import static com.kreidev.cmhoverpacks.Hoverpacks.resLoc;
 
@@ -15,15 +22,46 @@ public class FlightHandler {
     public static final AttributeModifier FLIGHT_MODIFIER = new AttributeModifier(resLoc("hoverpack_flight"), 1.0,
             AttributeModifier.Operation.ADD_VALUE);
 
+    public static final Map<UUID, Vec3> PREVIOUS_POSITIONS = new HashMap<>();
+    public static final Map<UUID, Double> DECIMAL_DECREMENTS = new HashMap<>();
+
+    public static final float AIR_PER_BLOCK = 1F;  // TODO: config
+    public static final float AIR_PER_SECOND = 3F;  // TODO: config
+
+    public static void updateTankAir(Player player, ItemStack hoverpack) {
+        UUID id = player.getUUID();
+        PREVIOUS_POSITIONS.putIfAbsent(id, player.position());
+        DECIMAL_DECREMENTS.putIfAbsent(id, 0.0);
+
+        Vec3 delta = player.position().subtract(PREVIOUS_POSITIONS.get(id));
+        delta = delta.y >= 0 ? delta : new Vec3(delta.x, 0, delta.z);  // Don't count down direction for fuel cost
+
+        double remainingDecrement = DECIMAL_DECREMENTS.get(id) + delta.length() * AIR_PER_BLOCK + AIR_PER_SECOND/2;
+        int decrement = (int) remainingDecrement;
+        HoverpackItem.setRemainingAir(hoverpack, BacktankItem.getRemainingAir(hoverpack) - decrement);
+        remainingDecrement -= decrement;
+
+        DECIMAL_DECREMENTS.put(id, remainingDecrement);
+        PREVIOUS_POSITIONS.put(id, player.position());
+    }
+
     public static void onPlayerTick(PlayerTickEvent.Post event) {
         Player player = event.getEntity();
         Level level = player.level();
 
         if (!level.isClientSide) {
-            if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof HoverpackItem) {
+            if (level.getGameTime() % 10 != 2) return;
+            ItemStack hoverpack = player.getItemBySlot(EquipmentSlot.CHEST);
+
+            if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof HoverpackItem
+                    && BacktankItem.getRemainingAir(hoverpack) > 0) {
                 AttributeInstance flightAttr = player.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
                 if (flightAttr != null && !flightAttr.hasModifier(FLIGHT_MODIFIER.id())) {
                     flightAttr.addTransientModifier(FLIGHT_MODIFIER);
+                }
+
+                if (player.getAbilities().flying) {
+                    updateTankAir(player, hoverpack);
                 }
             } else {
                 AttributeInstance flightAttr = player.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
--- src/main/java/com/kreidev/cmhoverpacks/HoverpackItem.java
+++ src/main/java/com/kreidev/cmhoverpacks/HoverpackItem.java
@@ -1,9 +1,11 @@
 package com.kreidev.cmhoverpacks;
 
+import com.simibubi.create.AllDataComponents;
 import com.simibubi.create.content.equipment.armor.BacktankItem;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
 
 import java.util.function.Supplier;
 
@@ -13,4 +15,7 @@ public class HoverpackItem extends BacktankItem {
         super(material, properties, textureLoc, placeable);
     }
 
+    public static void setRemainingAir(ItemStack stack, int air) {
+        stack.set(AllDataComponents.BACKTANK_AIR, Math.max(air, 0));
+    }
 }
