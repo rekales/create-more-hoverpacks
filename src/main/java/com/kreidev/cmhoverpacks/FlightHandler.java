81bc00016eae43de11233bd6161fb3a56d67f068
Krei <criketgun@gmail.com>
2026-02-28 12:16:50 +0800

    add particles

--- src/main/java/com/kreidev/cmhoverpacks/FlightHandler.java
+++ src/main/java/com/kreidev/cmhoverpacks/FlightHandler.java
@@ -1,6 +1,8 @@
 package com.kreidev.cmhoverpacks;
 
 import com.simibubi.create.content.equipment.armor.BacktankItem;
+import com.simibubi.create.foundation.particle.AirParticleData;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -53,6 +55,10 @@ public class FlightHandler {
             if (level.getGameTime() % 10 != 2) return;
             ItemStack hoverpack = player.getItemBySlot(EquipmentSlot.CHEST);
 
+            if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof BacktankItem) {
+                Hoverpacks.LOGGER.debug(BacktankItem.getRemainingAir(player.getItemBySlot(EquipmentSlot.CHEST))+"");
+            }
+
             if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof HoverpackItem
                     && BacktankItem.getRemainingAir(hoverpack) > 0) {
                 AttributeInstance flightAttr = player.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
@@ -67,6 +73,45 @@ public class FlightHandler {
                 AttributeInstance flightAttr = player.getAttribute(NeoForgeMod.CREATIVE_FLIGHT);
                 if (flightAttr != null && flightAttr.hasModifier(FLIGHT_MODIFIER.id())) {
                     flightAttr.removeModifier(FLIGHT_MODIFIER.id());
+                }
+            }
+        } else {
+            if (player.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof BacktankItem && player.getAbilities().flying) {
+                if (level.getGameTime()%2 == 0) {
+                    for (int i = 0; i < 2; i++) {  // i == 0 : right
+                        double xRand = (player.getRandom().nextDouble() - 0.5);
+                        double zRand = (player.getRandom().nextDouble() - 0.5);
+
+                        Vec3 spawnOffset = new Vec3(
+                                xRand * 0.65,
+                                0,
+                                zRand  * 0.65
+                        );
+                        spawnOffset = spawnOffset.xRot(Mth.DEG_TO_RAD * 45);
+                        if (i == 0)
+                            spawnOffset = spawnOffset.add(0.8,1.1,0.5);
+                        else {
+                            spawnOffset = spawnOffset.add(-0.8,1.1,0.5);
+                        }
+                        spawnOffset = spawnOffset.yRot( Mth.DEG_TO_RAD * (180-player.yBodyRot));
+
+                        Vec3 deltaVec = new Vec3(
+                                xRand * 1.5,
+                                -4,
+                                zRand * 1.5
+                        );
+                        deltaVec = deltaVec.xRot(Mth.DEG_TO_RAD * 45);
+                        if (i == 0)
+                            deltaVec = deltaVec.yRot( Mth.DEG_TO_RAD * (170-player.yBodyRot));
+                        else {
+                            deltaVec = deltaVec.yRot( Mth.DEG_TO_RAD * (190-player.yBodyRot));
+                        }
+
+                        level.addParticle(new AirParticleData(1f, 0.3F),
+                                player.getX() + spawnOffset.x, player.getY() + spawnOffset.y, player.getZ() + spawnOffset.z,
+                                deltaVec.x, deltaVec.y, deltaVec.z
+                        );
+                    }
                 }
             }
         }
